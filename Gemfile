8d24df6ac7b4b9a05fa36588f7e7dbd0cf22f614
Kenichiro Yasuda <keyasuda@users.noreply.github.com>
2025-07-10 15:35:50 +0900

    テスト追加

--- Gemfile
+++ Gemfile
@@ -6,3 +6,8 @@ git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 gemspec
 
 gem 'concurrent-ruby', '1.3.4'
+
+gem "activemodel", "~> 6.0.0"
+gem "activesupport", "~> 6.0.0"
+
+gem "timecop", "~> 0.9.10"
---
+++ spec/rails_helper.rb
@@ -0,0 +1,9 @@
+require 'active_model'
+require 'active_support/all'
+require 'timecop'
+require_relative '../lib/validators/date_format_validator'
+
+Time.zone = 'Tokyo'
+
+# This file is intentionally left blank.
+# It's a placeholder for RSpec's rails_helper.
